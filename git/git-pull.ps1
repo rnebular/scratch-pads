f2bbad6d3c731bea5fbd03fbba032275b7d203da
Jon Byrum <jon.byrum@gettyimages.com>
2022-01-19 08:28:02 -0800

    really lazy, made git-pull a function

--- git/git-pull.ps1
+++ git/git-pull.ps1
@@ -1,28 +1,30 @@
 # I am a lazy engineer and don't want to do this stuff all the time
 
-# git pull for every sub-folder of current location
-$start_location = Get-location
+function Pull-GitFolders {
+    # git pull for every sub-folder of current location
+    $start_location = Get-location
 
-$subfolders = Get-ChildItem -Directory
+    $subfolders = Get-ChildItem -Directory
 
-foreach ($folder in $subfolders) {
-    $foldername = $folder.Name
-    Write-Output "Pulling updates for $foldername"
-    Set-Location $folder
-    try {
-        $current_branches = git branch
-        if ($current_branches -contains "main") {
-            git checkout main
-            git pull
-        } else {
-            git checkout master
-            git pull
+    foreach ($folder in $subfolders) {
+        $foldername = $folder.Name
+        Write-Output "Pulling updates for $foldername"
+        Set-Location $folder
+        try {
+            $current_branches = git branch
+            if ($current_branches -like "*main") {
+                git checkout main
+                git pull
+            } else {
+                git checkout master
+                git pull
+            }
+        } catch {
+            Write-Output "Git command(s) failed, perhaps this folder isn't a git clone?"
+            Write-Output $Error[0]
         }
-    } catch {
-        Write-Output "Git command(s) failed, perhaps this folder isn't a git clone?"
-        Write-Output $Error[0]
+        Set-Location $start_location
     }
-    Set-Location $start_location
 }
 
 # end of line
