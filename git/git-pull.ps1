7c42c6952f5163bc3fd7f893d0ab5227cda1890b
Jon Byrum <jon.byrum@gettyimages.com>
2022-04-18 15:05:23 -0700

    updated git-pull to factor in folders that are NOT a git project.

--- git/git-pull.ps1
+++ git/git-pull.ps1
@@ -8,23 +8,29 @@ function Pull-GitFolders {
 
     foreach ($folder in $subfolders) {
         $foldername = $folder.Name
-        Write-Output "Pulling updates for $foldername"
         Set-Location $folder
-        try {
-            $current_branches = git branch
-            if ($current_branches -like "*main") {
-                git checkout main
-                git pull
-            } else {
-                git checkout master
-                git pull
+        $is_git_project = Test-Path ".git"
+        if ($is_git_project -eq "true") {
+            Write-Output "Pulling updates for $foldername"
+            try {
+                $current_branches = git branch
+                if ($current_branches -like "*main") {
+                    git checkout main
+                    git pull
+                } else {
+                    git checkout master
+                    git pull
+                }
+            } catch {
+                Write-Output "Git command(s) failed, perhaps this folder isn't a git clone?"
+                Write-Output $Error[0]
             }
-        } catch {
-            Write-Output "Git command(s) failed, perhaps this folder isn't a git clone?"
-            Write-Output $Error[0]
+        } else {
+            Write-Output "$folder is just a folder, not a real git project. There may be git projects under this sub-folder."
         }
         Set-Location $start_location
     }
 }
+
 
 # end of line
